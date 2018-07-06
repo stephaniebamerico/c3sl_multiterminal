ff4476f693f5019fedf6634ed353fabaadc96f7a
Guilherme Becker Agge <gba17@inf.ufpr.br>
2018-07-06 10:18:53 -0300

    SCRUM#580: Refactor the deploy code

--- .deploy.sh
+++ .deploy.sh
@@ -5,19 +5,28 @@
 
 apt-get update && apt-get install -y dput ssh
 
-eval $(ssh-agent -s)
+eval "$(ssh-agent -s)"
 ssh-add <(echo "$PRIV_KEY")
 
 echo "Deploy job name: $CI_JOB_NAME"
 
-if [ "$CI_JOB_NAME" = "deploy_for_testing" ]; then
+if [ "$CI_JOB_NAME" = "deploy_for_unstable" ]; then
     dput -u -f --config=dput.cf repo build/*.changes
-elif [ "$CI_JOB_NAME" = "deploy_for_stable" ]; then
+
+else
+    if [ "$CI_JOB_NAME" = "deploy_for_testing" ]; then
+       REPO_SRC="unstable"
+       REPO_TGT="testing"
+    elif [ "$CI_JOB_NAME" = "deploy_for_stable" ]; then
+       REPO_SRC="testing"
+       REPO_TGT="stable"
+    fi
+
     CHANGELOG="package/debian/changelog"
     LINE=$(head -n 1 $CHANGELOG)
-    PACKAGE=$(echo $LINE | cut -d' ' -f1)
-    VERSION=$(echo $LINE | cut -d' ' -f2 | grep -o -E '[0-9]*\.[0-9]*\.[0-9]*')
+    PACKAGE=$(echo "$LINE" | cut -d' ' -f1)
+    VERSION=$(echo "$LINE" | cut -d' ' -f2 | grep -o -E '[0-9]*\.[0-9]*\.[0-9]*')
+    REPREPRO_CMD="reprepro -Vb /home/repository/www/le6 copysrc $REPO_TGT $REPO_SRC $PACKAGE $VERSION"
 
-    REPREPRO_CMD="reprepro -Vb /home/repository/www/le6 copysrc stable testing $PACKAGE $VERSION"
-    ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa repository@repo.c3sl.ufpr.br "$REPREPRO_CMD"
-fi  
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null repository@repo.c3sl.ufpr.br "$REPREPRO_CMD"
+fi
